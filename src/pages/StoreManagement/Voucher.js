153a9c622ba57ec8564f3529cba148afbdb358bf
Phuong Tran Doan <phuongit1803@gmail.com>
2022-12-07 16:48:55 +0700

    Voucher Management

--- src/pages/StoreManagement/Voucher.js
+++ src/pages/StoreManagement/Voucher.js
@@ -6,6 +6,7 @@ import ThoudsandSeparator from "../../components/ThousandSeparator";
 import VoucherStatus from "../../components/VoucherStatus";
 import { HiOutlineRefresh } from "react-icons/hi";
 import { AiOutlineDelete } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
 
 const sortOptions = [
     { value: "1", label: "Mới nhất trước" },
@@ -22,14 +23,21 @@ const filterOptions = [
     { value: "5", label: "Đã hết hạn" },
 ];
 
+const unitOptions = [
+    { value: 0, label: "%" },
+    { value: 1, label: "VNĐ" },
+];
+
 const today = new Date();
 
 const Voucher = () => {
     const [sortOption, setSortOption] = useState(null);
     const [filterOption, setFilterOption] = useState(null);
-    const [showAddModal, setShowAddModal] = useState(true);
+    const [showAddModal, setShowAddModal] = useState(-1);
+    // -1 : hide, 0: add modal, 1 2 ....: edit voucher id
     const [data, setData] = useState([
         {
+            id: 1,
             code: "VOUCHER001",
             start: today,
             end: today,
@@ -42,6 +50,7 @@ const Voucher = () => {
             status: 2, //1: chưa đến ngày, 2: có thể dùng, 3: đã hết lượt, 4: đã hết hạn
         },
         {
+            id: 2,
             code: "VOUCHER002",
             start: today,
             end: today,
@@ -54,6 +63,7 @@ const Voucher = () => {
             status: 1, //1: chưa đến ngày, 2: có thể dùng, 3: đã hết lượt, 4: đã hết hạn
         },
         {
+            id: 3,
             code: "VOUCHER003",
             start: today,
             end: today,
@@ -66,6 +76,7 @@ const Voucher = () => {
             status: 3, //1: chưa đến ngày, 2: có thể dùng, 3: đã hết lượt, 4: đã hết hạn
         },
         {
+            id: 4,
             code: "VOUCHER004",
             start: today,
             end: today,
@@ -78,6 +89,7 @@ const Voucher = () => {
             status: 4, //1: chưa đến ngày, 2: có thể dùng, 3: đã hết lượt, 4: đã hết hạn
         },
         {
+            id: 5,
             code: "VOUCHER005",
             start: today,
             end: today,
@@ -90,6 +102,7 @@ const Voucher = () => {
             status: 2, //1: chưa đến ngày, 2: có thể dùng, 3: đã hết lượt, 4: đã hết hạn
         },
         {
+            id: 6,
             code: "VOUCHER006",
             start: today,
             end: today,
@@ -103,8 +116,12 @@ const Voucher = () => {
         },
     ]);
 
+    const VoucherItemOnClick = (voucher) => {
+        setShowAddModal(voucher.id);
+    };
+
     useEffect(() => {
-        if (showAddModal) document.body.style.overflow = "hidden";
+        if (showAddModal >= 0) document.body.style.overflow = "hidden";
         else document.body.style.overflow = "unset";
     }, [showAddModal]);
 
@@ -114,7 +131,10 @@ const Voucher = () => {
                 Quản Lý<span className="green-text"> Mã Giảm Giá</span>
             </span>
             <div className="top-buttons">
-                <button className="primary-button add-voucher-button">
+                <button
+                    className="primary-button add-voucher-button"
+                    onClick={() => setShowAddModal(0)}
+                >
                     <MdAdd />
                     Thêm mới
                 </button>
@@ -174,7 +194,10 @@ const Voucher = () => {
                 <div className="voucher-list">
                     {data.length > 0 ? (
                         data.map((item) => (
-                            <div className="voucher-item">
+                            <div
+                                className="voucher-item"
+                                onClick={() => VoucherItemOnClick(item)}
+                            >
                                 <span className="column-1">{item.code}</span>
                                 <span className="column-2">
                                     {item.start.toLocaleDateString()} -{" "}
@@ -215,22 +238,237 @@ const Voucher = () => {
                     )}
                 </div>
             </div>
-            {showAddModal && <AddVoucherModal />}
+            {showAddModal >= 0 && (
+                <AddVoucherModal
+                    id={showAddModal}
+                    voucher={data.find((item) => item.id === showAddModal)}
+                    closeFunction={() => setShowAddModal(-1)}
+                />
+            )}
         </div>
     );
 };
 
-const AddVoucherModal = (id) => {
-    return (
-        <div className="AddVoucherModal">
-            <div className="container">
-                <span className="title-text">Thêm Mã Giảm Giá</span>
-                <div className="row">
-                    <input placeholder="Mã giảm giá" />
+const AddVoucherModal = ({ id, voucher, closeFunction }) => {
+    const [unit, setUnit] = useState(unitOptions[0]);
+
+    useEffect(() => {
+        if (id > 0 && voucher.value.isPercent === false) {
+            setUnit(unitOptions[1]);
+        }
+    });
+
+    console.log("unit: ", unit);
+
+    if (id === 0)
+        return (
+            <div className="AddVoucherModal">
+                <div className="container">
+                    <button className="close-button" onClick={closeFunction}>
+                        <IoMdClose className="icon" />
+                    </button>
+                    <span className="title-text">
+                        Thêm <span className="green-text">Mã Giảm Giá</span>{" "}
+                    </span>
+                    <div className="row">
+                        <span className="label">Mã giảm giá</span>
+                        <div className="info">
+                            <input className="code-input" type="text" />
+                            <span
+                                className="label"
+                                style={{
+                                    width: "fit-content",
+                                    margin: "0 12px 0 30px",
+                                }}
+                            >
+                                Số lượt
+                            </span>
+                            <input
+                                className="count-input"
+                                type="number"
+                                placeholder="Số lượt"
+                            />
+                        </div>
+                    </div>
+                    <div className="row" style={{ marginTop: 0 }}>
+                        <span className="warning">
+                            * Mã giảm giá gồm 6-10 kí tự viết liền, không được
+                            trùng với Mã giảm giá khác
+                        </span>
+                    </div>
+                    <div className="row">
+                        <span className="label">Ngày bắt đầu</span>
+                        <div className="info">
+                            <input
+                                className="date-input"
+                                type="datetime-local"
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <span className="label">Ngày kết thúc</span>
+                        <div className="info">
+                            <input
+                                className="date-input"
+                                type="datetime-local"
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <span className="label">Giá trị</span>
+                        <div className="info">
+                            <input
+                                className="value-input"
+                                type="number"
+                                placeholder="Giá trị"
+                            />
+                            <Select
+                                className="unit-select"
+                                defaultValue={unit}
+                                onChange={setUnit}
+                                options={unitOptions}
+                                theme={(theme) => ({
+                                    ...theme,
+                                    borderRadius: "8px",
+                                    colors: {
+                                        ...theme.colors,
+                                        primary: "var(--primary-color)",
+                                    },
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <span className="label">Giảm tối đa</span>
+                        <div className="info">
+                            <input
+                                className="max-input"
+                                type="number"
+                                placeholder="Giảm tối đa"
+                                disabled={unit === unitOptions[0]}
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <button className="add-button primary-button">
+                            Thêm Mã Giảm Giá
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    else
+        return (
+            <div className="AddVoucherModal">
+                <div className="container">
+                    <button className="close-button" onClick={closeFunction}>
+                        <IoMdClose className="icon" />
+                    </button>
+                    <span className="title-text">
+                        Cập Nhật <span className="green-text">Mã Giảm Giá</span>{" "}
+                    </span>
+                    <div className="row">
+                        <span className="label">Mã giảm giá</span>
+                        <div className="info">
+                            <input
+                                className="code-input"
+                                type="text"
+                                placeholder="VD: VOUCHER012"
+                                value={voucher.code}
+                            />
+                            <span
+                                className="label"
+                                style={{
+                                    width: "fit-content",
+                                    margin: "0 12px 0 30px",
+                                }}
+                            >
+                                Số lượt
+                            </span>
+                            <input
+                                className="count-input"
+                                type="number"
+                                placeholder="Số lượt"
+                                value={voucher.count}
+                            />
+                        </div>
+                    </div>
+                    <div className="row" style={{ marginTop: 0 }}>
+                        <span className="warning">
+                            * Mã giảm giá gồm 6-10 kí tự viết liền, không được
+                            trùng với Mã giảm giá khác
+                        </span>
+                    </div>
+                    <div className="row">
+                        <span className="label">Ngày bắt đầu</span>
+                        <div className="info">
+                            <input
+                                className="date-input"
+                                type="datetime-local"
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <span className="label">Ngày kết thúc</span>
+                        <div className="info">
+                            <input
+                                className="date-input"
+                                type="datetime-local"
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <span className="label">Giá trị</span>
+                        <div className="info">
+                            <input
+                                className="value-input"
+                                type="number"
+                                placeholder="Giá trị"
+                                value={voucher.value.money}
+                            />
+                            <Select
+                                className="unit-select"
+                                value={unit}
+                                onChange={setUnit}
+                                options={unitOptions}
+                                theme={(theme) => ({
+                                    ...theme,
+                                    borderRadius: "8px",
+                                    colors: {
+                                        ...theme.colors,
+                                        primary: "var(--primary-color)",
+                                    },
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <span className="label">Giảm tối đa</span>
+                        <div className="info">
+                            <input
+                                className="max-input"
+                                type="number"
+                                placeholder="Giảm tối đa"
+                                value={
+                                    voucher.value.max > 0
+                                        ? voucher.value.max
+                                        : ""
+                                }
+                                disabled={unit === unitOptions[1]}
+                            />
+                        </div>
+                    </div>
+                    <div className="row save-form">
+                        <button className="add-button primary-button save-form">
+                            Lưu thông tin
+                        </button>
+                        <button className="add-button primary-button save-form">
+                            Xoá Mã
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
 };
 
 export default Voucher;

aaea234830991c73d10de0f4cfc9990736245ef4
tamdoduc <82079157+tamdoduc@users.noreply.github.com>
2022-12-27 12:04:36 +0700

    Update Evalute
    + fullName
    + imageURL

--- Server/routes/evalute.js
+++ Server/routes/evalute.js
@@ -2,79 +2,81 @@ const express = require("express");
 const router = express.Router();
 
 const Evalute = require("../models/evalutes");
+const Account = require("../Models/accounts");
 
 // @route GET api/evalutes
 // @desc get evalute
 // @access Public
 router.get("/", async (req, res) => {
-    try {
-        const evalutes = await Evalute.find({ productId: req.query.productId });
-        res.json({ success: true, evalutes });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: " Internal server error",
-        });
-    }
+  try {
+    const evalutes = await Evalute.find({ productId: req.query.productId });
+    res.json({ success: true, evalutes });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: " Internal server error",
+    });
+  }
 });
 
 // @route POST api/evalutes/create
 // @desc create color
 // @access Public
 router.post("/create", async (req, res) => {
-    const { accountId, productId, name, star } = req.body;
+  const { accountId, productId, describe, star } = req.body;
+  try {
+    if (!describe || !star || !productId || !accountId)
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing information" });
 
-    if (!name || !star || !productId || !accountId)
-        return res
-            .status(400)
-            .json({ success: false, message: "Missing information" });
-    try {
-        // Check for existing evalute
-        const evalute = await Evalute.findOne({ productId, accountId });
-        if (evalute)
-            return res
-                .status(400)
-                .json({ success: false, message: "Already exist evalute" });
+    // All Good
+    const account = await Account.findOne({ _id: accountId });
+    const fullName = account.fullName;
+    const imageURL = account.imageURL;
 
-        // All Good
-        const newEvalute = new Evalute({ name, productId, accountId, star });
-        await newEvalute.save();
-        return res
-            .status(200)
-            .json({ success: true, message: "Created evalute" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: " Internal server error",
-        });
-    }
+    const newEvalute = new Evalute({
+      describe,
+      productId,
+      accountId,
+      star,
+      fullName,
+      imageURL,
+    });
+    await newEvalute.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Created evalute", newEvalute });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: " Internal server error",
+    });
+  }
 });
 
 // @route Delete api/evalutes
 // @desc delete evalute
 // @access Public
 router.delete("/", async (req, res) => {
-    try {
-        const evalutes = await Evalute.find({
-            productId: req.productId,
-            accountId: req.accountId,
-        });
-        const deleteEvalute = await evalutes.findAndDelete(evalutes);
-        if (!deleteEvalute)
-            res.status(500).json({
-                success: false,
-                message: "Evalute not found",
-            });
-        res.json({ success: true, message: "Deleted evalute" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: " Internal server error",
-        });
-    }
+  const { evaluteId } = req.body;
+  try {
+    const deleteEvalute = await Evalute.findByIdAndDelete({ _id: evaluteId });
+    if (!deleteEvalute)
+      res.status(500).json({
+        success: false,
+        message: "Evalute not found",
+      });
+    else res.json({ success: true, message: "Deleted evalute" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: " Internal server error",
+    });
+  }
 });
 
 module.exports = router;

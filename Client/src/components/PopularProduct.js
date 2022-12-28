bca8ecdace32d627d664150f348e095b3b3763dc
Phuong Tran Doan <phuongit1803@gmail.com>
2022-12-28 12:49:11 +0700

    Home / Popular products

--- Client/src/components/PopularProduct.js
+++ Client/src/components/PopularProduct.js
@@ -1,136 +1,160 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PopularProduct.css";
 import { BiCategory } from "react-icons/bi";
 import { AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
 
-import i5stars from "../assets/images/reviews/5.png";
-
-import pi1 from "../assets/ExampleProduct/iPhone/1.png";
-import pi2 from "../assets/ExampleProduct/binhgiunhiet/binh-giu-nhiet-lock-lock-name-tumbler-lhc4125b-7.jpg";
-import pi3 from "../assets/ExampleProduct/dongho/1.jpeg";
-import pi4 from "../assets/ExampleProduct/giay/1.jpg";
-import pi5 from "../assets/ExampleProduct/kemchongnang/00013491-kem-chong-nang-la-roche-posay-kiem-soat-dau-spf50-6561-634c_large.webp";
-import pi6 from "../assets/ExampleProduct/laptop/gsmarena001-1653379584969-16533795856231394043061.webp";
-import pi7 from "../assets/ExampleProduct/nuochoa/6.jpg";
-import pi8 from "../assets/ExampleProduct/sach/2.jpg";
 import ThousandSeparator from "./ThousandSeparator";
-
-const data = [
-    {
-        name: "Apple iPhone 14 Pro Max 1TB Chính Hãng",
-        image: pi1,
-        price: 35000000,
-        salePrice: 38990000,
-    },
-    {
-        name: "Sách Dám mơ lớn, đừng hoài phí tuổi trẻ - Lư Tư Hạo",
-        image: pi8,
-        price: 96000,
-        salePrice: 132000,
-    },
-
-    {
-        name: "Đồng Hồ Thông Minh Xiaomi Mi Watch",
-        image: pi3,
-        price: 350000,
-        salePrice: 389900,
-    },
-    {
-        name: "Giày Da Thể Thao Nam Cao Cấp - Bảo Hành 12 Tháng",
-        image: pi4,
-        price: 450000,
-        salePrice: 489000,
-    },
-    {
-        name: "Kem Chống Nắng La Roche-Posay Kiểm Soát Dầu SPF50+",
-        image: pi5,
-        price: 350000,
-        salePrice: 389000,
-    },
-    {
-        name: "Bình Giữ Nhiệt Lock&Lock 750ML Chính hãng",
-        image: pi2,
-        price: 169000,
-        salePrice: 230000,
-    },
-    {
-        name: "Nước Hoa Pháp Queen Cao Cấp 50ml - Lưu hương 24h",
-        image: pi7,
-        price: 2500000,
-        salePrice: 3690000,
-    },
-    {
-        name: "Laptop Huawei Matebook D14 - SSD 512GB",
-        image: pi6,
-        price: 15000000,
-        salePrice: 18990000,
-    },
-];
+import Loading from "./Loading";
+import GetStarImage from "./GetStarImage";
+import axios from "axios";
 
 function PopularProduct() {
-    return (
-        <div className="PopularProduct content">
-            <div className="popular-head">
-                <p className="title-text">
-                    Sản phẩm <span className="green-text">Phổ biến</span>
-                </p>
-                <div className="popular-filter">
-                    <BiCategory className="popular-filter-icon" />
-                    <select className="popular-filter-selector">
-                        <option value={"All"}>Tất cả danh mục</option>
-                        <option value={"Điện thoại"}>Điện thoại</option>
-                        <option value={"Laptop"}>Laptop</option>
-                        <option value={"Thời trang nam"}>Thời trang nam</option>
-                        <option value={"Thời trang nữ"}>Thời trang nữ</option>
-                        <option value={"Trang sức"}>Trang sức</option>
-                        <option value={"Thiết bị điện tử"}>
-                            Thiết bị điện tử
-                        </option>
-                        <option value={"Nhà bếp"}>Nhà bếp</option>
-                        <option value={"Giày nam"}>Giày nam</option>
-                        <option value={"Giày nữ"}>Giày nữ</option>
-                        <option value={"Sách"}>Sách</option>
-                        <option value={"Đồng hồ"}>Đồng hồ</option>
-                        <option value={"Cho bé"}>Cho bé</option>
-                        <option value={"Sức khoẻ"}>Sức khoẻ</option>
-                        <option value={"Mỹ phẩm"}>Mỹ phẩm</option>
-                        <option value={"Dụng cụ gia đình"}>
-                            Dụng cụ gia đình
-                        </option>
-                        <option value={"Khác"}>Khác</option>
-                    </select>
+    const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState();
+    const [widthPercent, setWidthPercent] = useState(100);
+    const [widthPercentItem, setWidthPercentItem] = useState(22);
+
+    useEffect(() => {
+        setIsLoaded(false);
+        axios
+            .get("http://localhost:5000/api/products/allPopulate", {
+                params: {
+                    count: 8,
+                    accountId: localStorage.getItem("accountID"),
+                },
+            })
+            .then((res) => {
+                if (res.data.products.length === 2) {
+                    setWidthPercent(60);
+                    setWidthPercentItem(40);
+                } else if (res.data.products.length === 6) {
+                    setWidthPercentItem(25.1);
+                }
+                setData(res.data.products);
+                console.log("res.data.products: ", res.data.products);
+                setIsLoaded(true);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    if (isLoaded)
+        return (
+            <div className="PopularProduct content">
+                <div className="popular-head">
+                    <p className="title-text">
+                        Sản phẩm <span className="green-text">Phổ biến</span>
+                    </p>
+                    <div className="popular-filter">
+                        <BiCategory className="popular-filter-icon" />
+                        <select className="popular-filter-selector">
+                            <option value={"All"}>Tất cả danh mục</option>
+                            <option value={"Điện thoại"}>Điện thoại</option>
+                            <option value={"Laptop"}>Laptop</option>
+                            <option value={"Thời trang nam"}>
+                                Thời trang nam
+                            </option>
+                            <option value={"Thời trang nữ"}>
+                                Thời trang nữ
+                            </option>
+                            <option value={"Trang sức"}>Trang sức</option>
+                            <option value={"Thiết bị điện tử"}>
+                                Thiết bị điện tử
+                            </option>
+                            <option value={"Nhà bếp"}>Nhà bếp</option>
+                            <option value={"Giày nam"}>Giày nam</option>
+                            <option value={"Giày nữ"}>Giày nữ</option>
+                            <option value={"Sách"}>Sách</option>
+                            <option value={"Đồng hồ"}>Đồng hồ</option>
+                            <option value={"Cho bé"}>Cho bé</option>
+                            <option value={"Sức khoẻ"}>Sức khoẻ</option>
+                            <option value={"Mỹ phẩm"}>Mỹ phẩm</option>
+                            <option value={"Dụng cụ gia đình"}>
+                                Dụng cụ gia đình
+                            </option>
+                            <option value={"Khác"}>Khác</option>
+                        </select>
+                    </div>
+                </div>
+                <div
+                    className="product-list"
+                    style={{ width: `${widthPercent}%` }}
+                >
+                    {data.map((item) => (
+                        <ProductCard
+                            item={item}
+                            widthPercentItem={widthPercentItem}
+                        />
+                    ))}
                 </div>
             </div>
-            <div className="product-list">
-                {data.map((item) => (
-                    <div className="product-item">
-                        <img className="product-img" src={item.image} alt="" />
-                        <div className="product-info">
-                            <p className="product-name">{item.name}</p>
-                            <div className="product-star">
-                                <img src={i5stars} alt="" />
-                                <span>(4.0)</span>
-                            </div>
-                            <div className="product-price-container">
-                                <span className="product-sale-price">
-                                    {ThousandSeparator(item.price)}
-                                </span>
-                                <span className="product-old-price">
-                                    {ThousandSeparator(item.salePrice)}
-                                </span>
-                            </div>
-                            <div className="product-buttons">
-                                <button className="product-add-to-cart-button">
-                                    <AiOutlineShoppingCart className="product-add-to-cart-icon" />
-                                    Thêm Vào Giỏ
-                                </button>
-                                <button className="product-add-to-wishlist-button">
-                                    <AiOutlineHeart className="product-add-to-wishlist-icon" />
-                                </button>
-                            </div>
-                        </div>
+        );
+    else return <Loading />;
+}
+
+function ProductCard({ item, widthPercentItem }) {
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    return (
+        <div className="product-item" style={{ width: `${widthPercentItem}%` }}>
+            <img className="product-img" src={item.imageURLs[0]} alt="" />
+            <div className="product-info">
+                <div className="product-name-wrapper">
+                    <p className="product-name">{item.nameProduct}</p>
+                </div>
+                <div className="product-star">
+                    <img src={GetStarImage(item.countStar)} alt="" />
+                    <span>
+                        ({Math.round(parseFloat(item.countStar) * 10) / 10})
+                    </span>
+                </div>
+                <div className="product-price-container">
+                    <span className="product-sale-price">
+                        {ThousandSeparator(item.price)} đ
+                    </span>
+                    <span className="product-old-price">
+                        {item.salePrice > 0
+                            ? ThousandSeparator(item.salePrice) + " đ"
+                            : ""}
+                    </span>
+                </div>
+                <div className="product-buttons">
+                    <button className="product-add-to-cart-button">
+                        <AiOutlineShoppingCart className="product-add-to-cart-icon" />
+                        Thêm Vào Giỏ
+                    </button>
+                    <div className="add-to-wishlist-wrapper">
+                        <input
+                            type="checkbox"
+                            checked={isFavorite}
+                            id="favorite"
+                            name="favorite-checkbox"
+                            value="favorite-button"
+                            className="atw-input"
+                        />
+                        <label
+                            for="favorite"
+                            className="container"
+                            onClick={() => setIsFavorite(!isFavorite)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                style={{
+                                    width: "24",
+                                    height: "24",
+                                    viewBox: "0 0 24 24",
+                                    fill: "none",
+                                    stroke: "currentColor",
+                                    strokeWidth: "2",
+                                    strokeLinecap: "round",
+                                    strokeLinejoin: "round",
+                                }}
+                                className="feather feather-heart"
+                            >
+                                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                            </svg>
+                        </label>
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
